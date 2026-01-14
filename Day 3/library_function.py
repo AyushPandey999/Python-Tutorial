1d23ec541a81c52ea1cbbc99285ffdd5eded43c4
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:11:33 +0530

    Create library_function.py

---
+++ Day 3/library_function.py
@@ -0,0 +1,9 @@
+'''
+The library functions are the functions or collection of related modules whose task are predefined by the developers .
+It is possible to access the functionality but can't modify its original task . 
+
+There are three types of library function :- 
+1) Keywords .
+2) Operators .
+3) Inbuilt functions .
+'''
