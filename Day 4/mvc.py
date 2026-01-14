0765fae2a6e1625739f73f9600047ba66d36db4e
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:11:49 +0530

    Create mvc.py

---
+++ Day 4/mvc.py
@@ -0,0 +1,9 @@
+'''
+MVC is multiple variable creation means creating the multiple variables or assigning the multiple values in a single line .
+
+Syntax -> var1,var2,....,varN = val1,val2,......,valN
+
+Note -> No. of variable = No. of values.
+'''
+a,b=20,10
+print(a,b)
