cfd3cca5e5e7fde06a2245356ea3a7142a94ad0b
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:10:33 +0530

    Create what.py

---
+++ Day 1/what.py
@@ -0,0 +1,122 @@
+# What is Programming Language ?
+
+"""
+Programming is the process of creating sets of instruction that tell a computer how to perform specific task.
+
+Popular Programming Languages : C,C++,Java,Python...etc 
+"""
+
+# What is Python ?
+
+""" 
+Python is a High-Level Oject-Oriented programming language known for its simplicity and readability.
+Python is Created by Guido Van Rossum in 1991.
+"""
+
+# Features of Python ?
+
+""" 
+1) High-Level language.
+2) Object Oriented language.
+3) Interpreted language.
+4) Scripting language.
+5) Dynamic Typed language.
+6) Huge Set of Libraries and In-built Function.
+7) Easy to understand.
+8) Easy to learn.
+9) Platform Independent.
+10) Open-Source and Free.
+"""
+
+# Difference between high level , mid level , low level language ?
+
+"""" 
+High level language : The language which is easily understandable by human but needs translation for computers. ex : python,java..etc.
+
+Mid level language : The language which is partially understandable by human and computer. ex : c,c++.
+
+Low level language : The language which is easily understandable by computer but difficult for humans. ex : machine language,assembly language.
+"""
+
+# Object Oriented ?
+
+""" 
+Python support object oriented concepts such as classes , objects , inheritance , encapsulation , polymorphism . Object oriented helps
+organize code and makes it reusable and easier to maintain.
+"""
+
+# Intepreted language ?
+
+""" 
+The language which executes the code line by line and stop at the error . You don't need to compile your code before running it , which
+makes debuging and testing much faster.
+"""
+
+# Scripting Language ?
+""" 
+A scripting language is a type of programming language that use an interpreter as its translator and is mainly used to automates
+tasks or control other application.
+"""
+
+# Dynamic Type Language ?
+
+"""
+In Python , You don't need to declare variable type explicitly. it declare the variable type during run time.
+"""
+
+# Huge set of library and in-built function ?
+
+"""
+Python provides a large collection of built-in functions and libraries that help perform various tasks like math , data handling , 
+web development , and machine learning easily.
+"""
+
+# Platform Independent ?
+"""
+Python is a cross platform , meaning code is written on one OS like window can run on other like Linux without modification.
+"""
+
+# Open Source and Free ?
+
+""" 
+Python is free to download and use and its source code is open to developer so , developer can contribute , modify , or use it freely.
+"""
+
+# Story behind python ?
+
+"""
+In December 1989 , Guido van rossum started working on a new programming language while at CWI(centrum wiskunde & informatica) in 
+natherland and release it publicly in 1991 as name python . so , python name is not taken from snake it's taken from guido's favourite 
+show Monty Python's comedy Flying Circus which is telecast in bbc chennel because guido wants a unique , simple and easily redable name.
+"""
+
+# Career with python ?
+
+""" 
+Python is not only one of the most popular programming language in the world but it also offers great carrer opportunities . The demand
+for python developers is growing every year for web development , data science and analytics , software development.
+"""
+
+# Difference between Interpreter and compiler ?
+
+"""
+1)
+Interpreter : An Interpreter translates and executes a source code line by line as the code runs.
+
+Compiler : A Compiler translates the entire code into machine code before the program runs.
+
+2)
+Interpreter : An Interpreter executes line by line.
+
+Compiler : A Compiler executes entire program at once.
+
+3)
+Interpreter : An Interpreter easier to debug as it stop at the first error encountered.
+
+Compiler : A Compiler harder to debug because errors and reported after the entire code is compiled.
+
+4)
+Interpreter : Example -> Python , Ruby , JavaScript , and PHP.
+
+Compiler : Example -> C , C++ , Java , and Go.
+"""
