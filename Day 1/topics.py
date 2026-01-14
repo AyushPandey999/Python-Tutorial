46ca18672ef8a1f2fb8d816d954156099ff02b2d
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:06:38 +0530

    Create topics.py

---
+++ Day 1/topics.py
@@ -0,0 +1,6 @@
+# What is Programming Language .
+# What is Python .
+# Features of Python and Their Explaination .
+# Story behind python .
+# Career with python .
+# Difference between Interpreter and compiler .
