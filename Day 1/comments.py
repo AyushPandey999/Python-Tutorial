df24951d0bdc8a5189a33c3543ac33c06a045eb9
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:10:38 +0530

    Create comments.py

---
+++ Day 1/comments.py
@@ -0,0 +1,14 @@
+# Comments
+
+""" 
+1)Comments are used as notes or short description alond with code to increase its readability.
+2)Comments are ignored by python interpreter during execution.
+
+There are two types of comment :
+    1) Single line comment : Use to write short explaination or to skip a single line of code.
+                             Example : # Statement.
+    2) Multi line comment : Use to write multiple line of explaination or to skip multiple line of codes. 
+                             Example : ''' Statement ''''.
+                             
+Extra : 1) Inline comment.
+"""
