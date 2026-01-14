707323ddd232bff3658088c8a79245567d471db1
KRYVENIX <shivamraj2f@gmail.com>
2026-01-14 19:12:08 +0530

    Create identifiers.py

---
+++ Day 4/identifiers.py
@@ -0,0 +1,10 @@
+'''
+1) Identifiers is used to identify the variables, classes, modules...etc.
+   Identifiers are the names which is given to variables, classes, modules...etc.
+2) Rules of Identifiers :-
+    1) We can't use keyword as a identifier.
+    2) We can't start identifier with a number.
+    3) We can't provide space In the starting position or the middle position of an identifier.
+    4) We can't use special symbols in identifier except _ .
+'''
+name=10
