00467775910c58dcc2b0560f4b297b7e43659d12
KRYVENIX <shivamraj2f@gmail.com>
2026-01-15 20:04:23 +0530

    Create datatypes.py

---
+++ Day 5/datatypes.py
@@ -0,0 +1,22 @@
+''' 
+Datatype specifies the type and size of value stored in a variable and there are two types of datatypes :-
+    1) Single-Value Datatype / Individual Datatype :- 
+        1) When we are storing a single value in a single variable then that is called the single value datatype
+           or individual datatype.
+        2) There are two types of Single-Value Datatype :-
+            1) Numeric (Int , Float , Complex).
+            2) Boolean (True , False)
+    2) Multi-Value Datatype / Collection Datatype :-
+        1) When we are storing multiple values in a single variable then it is called the multi value datatype 
+           or collection datatype.
+        2) There are 5 types of Multi-Value Datatype :-
+            1) String.
+            2) List.
+            3) Tupple.
+            4) Set.
+            5) Dictionary.
+'''
+a=10,20,30
+# print(type(a))
+print(a)
+print(type(a))
